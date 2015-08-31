6b29bd04c3b9f57068f5c7560837fc618b6e45be
Sharon Wag <sharon.ran.wang@gmail.com>
2015-08-31 13:59:54 -0400

    created menu table

--- getaway/db/schema.rb
+++ getaway/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829195309) do
+ActiveRecord::Schema.define(version: 20150831174458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "restaurant_id"
@@ -26,12 +34,14 @@ ActiveRecord::Schema.define(version: 20150829195309) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "order_id"
     t.integer  "visit_id"
+    t.integer  "menu_item_id"
   end
 
+  add_index "restaurants", ["menu_item_id"], name: "index_restaurants_on_menu_item_id", using: :btree
   add_index "restaurants", ["order_id"], name: "index_restaurants_on_order_id", using: :btree
   add_index "restaurants", ["visit_id"], name: "index_restaurants_on_visit_id", using: :btree
 
@@ -56,6 +66,7 @@ ActiveRecord::Schema.define(version: 20150829195309) do
     t.datetime "updated_at",    null: false
   end
 
+  add_foreign_key "restaurants", "menu_items"
   add_foreign_key "restaurants", "orders"
   add_foreign_key "restaurants", "visits"
   add_foreign_key "users", "orders"
