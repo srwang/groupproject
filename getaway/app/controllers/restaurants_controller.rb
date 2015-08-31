365104bc53e56fa5bae5d8374e0d5ac40cf437af
fhcw31@gmail.com Wong <fhcw31@gmail.com>
2015-08-31 10:08:36 -0400

    first commit

--- getaway/app/controllers/restaurants_controller.rb
+++ getaway/app/controllers/restaurants_controller.rb
@@ -1,3 +1,19 @@
-class RestaurantController < ApplicationController
-	
+class RestaurantsController < ApplicationController
+# 	  restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 PUT    /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+		respond_to do |format|
+			format.html
+  			format.js
+  		end
+	end
+
 end
