ad4c78b2c8294c06e048a253861a020706f0aa02
Sharon Wag <sharon.ran.wang@gmail.com>
2015-09-01 16:49:10 -0400

    socket working

--- socket_server/app.js
+++ socket_server/app.js
@@ -1,45 +1,33 @@
 var WebSocketServer = require("ws").Server;
 var server = new WebSocketServer({port: 2000});
-client_count = 1;
 
 var clients = [];
 
 client_id = "2";
 
-
   server.on("connection",function(client) {
-
-    console.log("friend connected");
-    console.log(client_count)
-    client_count++
  
     client.on('message', function(message) {
-	var unique = true
-
-    	if (/[1-9]+/.test(message)) {
-    		clients.forEach(function(client){
-    			if (client.id == parseInt(message)) {
-    				unique = false
-    			} 
-    		})
-    		if (unique === true) {
-		    	var user= {id: message, connection: client}
-		    	clients.push(user)    			
-    		}
-    	} else {
-    		clients.forEach(function(client){
-    			if (client.id === client_id) {
-    				client.connection.send('hi there')
-    			}
-    		})
-    	}
-    	console.log(clients)    		
-
-
-
+        
+        if (/[1-9]+/.test(message)) {
+            var user= {id: message, connection: client}
+            clients.push(user)              
+        } else {
+            console.log("client id " + client_id)
+            clients.forEach(function(client){
+                if (client.id === client_id) {
+                    client.connection.send('hi there')
+                }
+            })
+        } 
+        console.log(clients)     
     });
 
   client.on("close", function() {
-
+    clients.forEach(function(item){
+        if (client === item.connection) {
+            clients.splice(clients.indexOf(item), 1)
+        }
+    })
   });
 });
