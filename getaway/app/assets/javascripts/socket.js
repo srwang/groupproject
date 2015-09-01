7bc4133a179e5d9b3b05d313b6b913a36eb1e13d
Sharon Wag <sharon.ran.wang@gmail.com>
2015-09-01 15:51:55 -0400

    testing socket

--- getaway/app/assets/javascripts/socket.js
+++ getaway/app/assets/javascripts/socket.js
@@ -1,7 +1,19 @@
-var client = new WebSocket("ws://localhost:2000");
-client.addEventListener("message", function(val){
-    // $('body').append(val.data)
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+var client = new WebSocket("ws://localhost:2000/");
+client.addEventListener("open", function(val){
+	client.send(readCookie("user_id"))
+	client.send("hi")
 });
-client.addEventListener('message', function(){
-	client.send('hey')
-})
+client.addEventListener("message", function(val){
+    $('body').append(val.data)
+});
--- socket_server/app.js
+++ socket_server/app.js
@@ -1,25 +1,45 @@
 var WebSocketServer = require("ws").Server;
 var server = new WebSocketServer({port: 2000});
+client_count = 1;
 
 var clients = [];
-var text = "";
+
+client_id = "2";
 
 
   server.on("connection",function(client) {
+
     console.log("friend connected");
-
-    
-   
+    console.log(client_count)
+    client_count++
+ 
     client.on('message', function(message) {
-    	console.log(message)
+	var unique = true
 
-	    client.send(message)
-	    text = message;
+    	if (/[1-9]+/.test(message)) {
+    		clients.forEach(function(client){
+    			if (client.id == parseInt(message)) {
+    				unique = false
+    			} 
+    		})
+    		if (unique === true) {
+		    	var user= {id: message, connection: client}
+		    	clients.push(user)    			
+    		}
+    	} else {
+    		clients.forEach(function(client){
+    			if (client.id === client_id) {
+    				client.connection.send('hi there')
+    			}
+    		})
+    	}
+    	console.log(clients)    		
+
+
+
     });
 
   client.on("close", function() {
 
   });
 });
-
-  ;
