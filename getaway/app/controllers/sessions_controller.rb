3ae1baea173a063c261ffd741586d74da17f86f5
Sharon Wag <sharon.ran.wang@gmail.com>
2015-09-02 10:51:26 -0400

    got sending message to admin working

--- getaway/app/controllers/sessions_controller.rb
+++ getaway/app/controllers/sessions_controller.rb
@@ -5,21 +5,22 @@ class SessionsController < ApplicationController
 
     def login
         render :index
-        @id = params[:id]
-        cookies[:user_id] = params[:id]
+        # @id = params[:id]
+        # cookies[:user_id] = params[:id]
+        # session[:user_id] = params[:id]
     end
 
     def create 
-
-        user = User.find_by({username: params[:username]})
+     user = User.find_by({username: params[:username]})
         restaurant = Restaurant.find(params[:id])
         if user && user.authenticate(params[:password])
             	session[:user_id] = user.id
                 cookies[:user_id] = user.id
+                cookies[:user_name] = user.name
             if user.account_type == 'admin'
                 redirect_to restaurant_path(restaurant)
             else
-            	redirect_to "/restaurants/#{params[:id]}/users/#{user.id}"
+            	redirect_to "/restaurants/#{params[:id]}/users/#{session[:user_id]}"
             end
         else
             redirect_to(:back)
@@ -30,7 +31,9 @@ class SessionsController < ApplicationController
 
     def  destroy 
         session[:user_id] = nil
-        cookie[:user_id] = nil
+        cookies[:user_id] = nil
+        cookies[:user_name] = nil
+        session[:in_line] = nil
         redirect_to '/login/1'
     end 
 
--- getaway/app/controllers/users_controller.rb
+++ getaway/app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
 	def show
 		if logged_in?
 			@restaurant = Restaurant.find(params[:restaurant_id])
-	  		@user = User.find(params[:id])
+	  		@user = User.find(session[:user_id])
 	  		@menu_items = MenuItem.where(restaurant_id:params[:restaurant_id])
 	  		@order = Order.new
 
 	  		if !(session[:in_line] == true)
-				Visit.create({user_id:params[:id], restaurant_id:params[:restaurant_id]})
+				Temp.create({user_id:params[:id], restaurant_id:params[:restaurant_id]})
 				session[:in_line] = true
 			end
-				@visit = Visit.last
+				@visit = Temp.last
 
 			respond_to do |format|
 				format.html
@@ -24,9 +24,18 @@ class UsersController < ApplicationController
 
 	def new
 		Order.create(order_params)
-		redirect_to "/restaurants/#{params[:restaurant_id]}/users/#{params[:order][:user_id]}"
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@user = User.new
+		admin = User.where(account_type: "admin")
+		puts admin
+		admins = []
+
+		admin.each do |admin|
+			admins.push(admin.id)
+		end
+
+		cookies[:admin_id] = admins
+		redirect_to "/restaurants/#{params[:restaurant_id]}/users/#{params[:order][:user_id]}"
 	end
 
 	def create
@@ -35,7 +44,9 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		
+		visit = Temp.where(user_id: session[:user_id])[0]
+		visit.destroy
+		redirect_to '/login/1'
 	end
 
 private
